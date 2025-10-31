88740dee64fa916284ac204a62bb1dc696a985eb
Antonyweryy <ivlievd156@gmail.com>
2025-10-31 21:14:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const WORKER_URL = "https://clientstate.teunser.workers.dev/";
+const WORKER_URL = "https://clientstate.teunser.workers.dev";
 
 async function initApp() {
   Telegram.WebApp.ready();
-  const user = Telegram.WebApp.initDataUnsafe.user;
-  if (!user) return document.getElementById("app").innerHTML = "Ошибка";
+  const tg = Telegram.WebApp;
 
-  // Проверяем роль
-  const res = await fetch(`${WORKER_URL}/getRole?user_id=${user.id}`);
+  const res = await fetch(`${WORKER_URL}/validate`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ initData: tg.initData }),
+  });
+
   const html = await res.text();
-
-  // Показываем нужный интерфейс
   document.getElementById("app").innerHTML = html;
 }
 
